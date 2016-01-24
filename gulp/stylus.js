d63b4ad427f5f3441cdf04bed99f74f4f5a5ce43
Carlos Santana Roldán <carlos@milkzoft.com>
2016-01-23 16:01:23 -0800

    Adding nib for Stylus
    Fixing indentation on package.json

--- gulp/stylus.js
+++ gulp/stylus.js
@@ -1,10 +1,15 @@
 var gulp = require('gulp');
 var stylus = require('gulp-stylus');
+var nib = require('nib');
 
 module.exports = function() {
     gulp.task('stylus', function() {
         gulp.src('./src/stylus/style.styl')
-            .pipe(stylus())
+            .pipe(stylus({
+                force: true,
+                compress: true,
+                use: nib()
+            }))
             .pipe(gulp.dest('./src/public/css/'));
     });
 };
