1572708a67b582a024c5086284cc0db02f8a4ee9
Carlos Santana Roldán <carlos@milkzoft.com>
2016-01-24 18:51:08 -0800

    Adding start & reset, difficulty (speed) & sounds

--- src/public/js/game.js
+++ src/public/js/game.js
@@ -4,6 +4,15 @@
     // Getting game board (ul).
     var game = document.querySelector('#game');
 
+    // Start game button
+    var startGame = document.querySelector('#start-game');
+
+    // Reset game button
+    var resetGame = document.querySelector('#reset-game');
+
+    // Difficulty
+    var difficulty = document.querySelector('#difficulty');
+
     // Getting all the moles (li).
     var moles = document.querySelectorAll('.game ul li');
 
@@ -13,34 +22,89 @@
     // Getting the mole hits (input).
     var gameHits = document.querySelector('#game-hits');
 
+    // Current active mole
+    var activeMole = -1;
+
+    // Buzz sound
+    var buzzSound = document.querySelector('#buzz-sound');
+
+    // Game ended
+    var gameEnded = false;
+
     // Setting the timer for 60 seconds.
-    var timer = new Timer({
+    var gameTimer = new Timer({
         seconds: 60,
         onTime: function(sec) {
+            // Updating the time every second
             gameTime.value = sec;
         },
         onTimeEnd: function() {
+            // Makes a buzz sound
+            buzzSound.play();
+
+            // Shows game over message
             alert('Game over!');
 
-            for (var i = 0; i < moles.length; i++) {
-                moles[i].className = 'active';
-            }
+            // Default values
+            resetDefaultValues();
+
+            gameEnded = true;
+
+            // Activating all moles
+            activeAllMoles(true);
+
+            // Disabling reset button.
+            toggleButtons();
+        },
+        getCurrentSeconds: function() {
+
         }
     });
 
+    // If the user clicks on start game, we start the timer.
+    startGame.addEventListener('click', function(event) {
+        // Disabling start button.
+        toggleButtons(true);
+
+        // Reset default values
+        resetDefaultValues();
+
+        // Rendering first mole
+        renderMole();
+
+        // Starts a new game
+        startNewGame();
+    }, false);
+
+    // If the user clicks on reset game, we reset the timer.
+    resetGame.addEventListener('click', function(event) {
+        // Reset default values
+        resetDefaultValues();
+
+        // Starts a new game
+        startNewGame(true);
+    }, false);
+
     // Adding click event to the game board (only if the time is grather than 0 seconds)
     game.addEventListener('click', function(event) {
-        if (gameTime.value > 0) {
+        if (gameTime.value == 0 && gameHits.value == 0 && activeMole === -1 || gameEnded) {
+            alert('Press Start Game button to play!');
+        } else if (gameTime.value > -1) {
             if (event.target.className === 'active') {
                 gameHits.value++;
+
+                if (activeMole !== -1 && gameTime.value == 60) {
+                    renderMole(true);
+                }
             }
         } else {
             alert('The game is over');
         }
     }, false);
 
-    // Start timer.
-    timer.start();
+    /**
+     * Functions
+     */
 
     // Timer function
     function Timer(options) {
@@ -58,6 +122,9 @@
 
         // onTimeEnd functions is executed when the time is 0 seconds.
         var onTimeEnd = options.onTimeEnd || function() {};
+
+        // Speed
+        var speed = options.speed || 0;
 
         // Decreasing the time second by second.
         function decrease() {
@@ -77,12 +144,92 @@
             clearInterval(timer);
             timer = 0;
             seconds = options.seconds;
-            timer = setInterval(decrease, 1000);
+            timer = setInterval(decrease, 1000 - speed);
         };
 
         // Stops the timer.
         this.stop = function () {
             clearInterval(timer);
         };
+    }
+
+    // Render mole
+    function renderMole() {
+        var tempMole = activeMole;
+
+        // Removing active class for previous mole
+        if (activeMole > -1) {
+            moles[activeMole].className = '';
+        }
+
+        activeMole = getRandomMole(4, 3);
+
+        if (tempMole === activeMole && activeMole > 0) {
+            activeMole--;
+        }
+
+        // Updating active mole
+        moles[activeMole].className = 'active';
+    }
+
+    // Random Mole
+    function getRandomMole(width, height) {
+        return Math.floor((Math.random() * (width * height)) + 1) - 1;
+    }
+
+    // Default values
+    function resetDefaultValues() {
+        activeAllMoles(false);
+        activeMole = -1;
+        gameHits.value = 0;
+        gameTime.value = 0;
+    }
+
+    // Show or hide all moles
+    function activeAllMoles(active) {
+        for (var i = 0; i < moles.length; i++) {
+            moles[i].className = active ? 'active' : '';
+        }
+    }
+
+    // Starts a new game
+    function startNewGame(stopMoleTimer) {
+        // Possible speeds
+        var speeds = {
+            0: 60,
+            200: 200,
+            500: 400,
+            900: 608
+        };
+
+        // Getting speed based on difficulty
+        var speed = difficulty.options[difficulty.selectedIndex].value;
+
+        var moleTimer = new Timer({
+            seconds: speeds[speed],
+            speed: speed,
+            onTime: function() {
+                if (!gameEnded) {
+                    renderMole();
+                }
+            }
+        });
+
+        // Start timer.
+        gameTimer.start();
+        moleTimer.start();
+
+        gameEnded = false;
+    }
+
+    // Toggle for start and reset buttons
+    function toggleButtons(start) {
+        startGame.className = '';
+        resetGame.className = 'no-display';
+
+        if (start) {
+            startGame.className = 'no-display';
+            resetGame.className = '';
+        }
     }
 })();
